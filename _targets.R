e38651bdefc3de7802aa171607e6bbd0eeb211d0
XavTheEcologist <quintalx2002@hotmail.com>
2024-03-28 10:56:50 -0400

    Ajout script Targets jusqu'à création des tables (avant SQL)

---
+++ _targets.R
@@ -0,0 +1,64 @@
+# ===========================================
+# _targets.R file
+# ===========================================
+# Dépendances
+library(targets)
+library(rmarkdown)
+library(RSQLite)
+#tar_option_set(packages = c())     Pour d'autres packages
+
+# Scripts R
+source("read_all_csv.R")
+source("correction_bird.R")
+source("format_col.R")
+source("function_clef.R")
+
+
+# Pipeline
+list(
+  
+  tar_target(    # Établissement du path pour lire les données brutes
+    name = path,
+    command = "C:/Users/Xavier/Documents/Université Sherbrooke/H2024/Méthode computationnelle/Projet/acoustique_oiseaux/oiseaux_test/Data"
+  ), 
+  
+  tar_target(    # Lecture des données et combiner en un dataframe
+    name = data, 
+    command = read.all.csv(path) 
+  ),   
+  
+  tar_target(    # Correction des données
+    name = data.cor,
+    command = correction.bird(data)
+  ),
+  
+  tar_target(    # Changement de format des données
+    name = data.final, 
+    command = type.format.col(data.cor) 
+  ),
+  
+  tar_target(    # Création table Espèces
+    name = esp,
+    command = clef_especes(data.final)
+  ),
+  
+  tar_target(    # Création table Time
+    name = time,
+    command = clef_temps(data.final)
+  ),
+  
+  tar_target(    # Création table Sites
+    name = site,
+    command = clef_site(data.final)
+  ),
+  
+  tar_target(    # Création table Observations
+    name = obs,
+    command = clef_obs(data.final, time)
+  )
+)
+
+
+
+
+
