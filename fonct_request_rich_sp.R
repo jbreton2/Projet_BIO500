00ff5e43a4beee8a631e71ba21b16204bf9040a9
XavTheEcologist <quintalx2002@hotmail.com>
2024-04-09 18:48:32 -0400

    Création de la fonction

--- fonct_request_rich_sp.R
+++ fonct_request_rich_sp.R
@@ -1,13 +1,18 @@
 
-request.rich <- "
-  SELECT observations.site_id, lat, COUNT(DISTINCT(valid_scientific_name)) AS richesse_sp
-  FROM observations
-  INNER JOIN sites ON observations.site_id = sites.site_id
-  GROUP BY sites.site_id
-  ORDER BY lat ASC;"
+table.rich.sp <- function(connexion.SQL){
+  
+  request.rich <- "
+    SELECT observations.site_id, lat, COUNT(DISTINCT(valid_scientific_name)) AS richesse_sp
+    FROM observations
+    INNER JOIN sites ON observations.site_id = sites.site_id
+    GROUP BY sites.site_id
+    ORDER BY lat ASC;"
+  
+  rich.sp <- dbGetQuery(connexion.SQL,request.rich)
+  
+  return(rich.sp)
 
-rich.spec <- dbGetQuery(con,request.rich)
-head(rich.spec)
+  }
 
 
 
