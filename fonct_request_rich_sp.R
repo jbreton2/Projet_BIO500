8a240f006623603d9338264fc1545b66cbc2f23d
XavTheEcologist <quintalx2002@hotmail.com>
2024-04-16 17:38:42 -0400

    modif pour les trois zones

--- fonct_request_rich_sp.R
+++ fonct_request_rich_sp.R
@@ -1,16 +1,46 @@
 
 table.rich.sp <- function(connexion.SQL){
   
-  request.rich <- "
+  request.rich.tot <- "
     SELECT observations.site_id, lat, COUNT(DISTINCT(valid_scientific_name)) AS richesse_sp
     FROM observations
     INNER JOIN sites ON observations.site_id = sites.site_id
     GROUP BY sites.site_id
     ORDER BY lat ASC;"
   
-  rich.sp <- dbGetQuery(connexion.SQL,request.rich)
+  rich.sp.tot <- dbGetQuery(connexion.SQL,request.rich.tot)
   
-  return(rich.sp)
+  request.rich.temp <- "
+    SELECT observations.site_id, lat, COUNT(DISTINCT(valid_scientific_name)) AS richesse_sp
+    FROM observations
+    INNER JOIN sites ON observations.site_id = sites.site_id
+    WHERE lat <= 48
+    GROUP BY sites.site_id
+    ORDER BY lat ASC;"
+  
+  rich.sp.temp <- dbGetQuery(connexion.SQL,request.rich.temp)
+  
+  request.rich.bor <- "
+    SELECT observations.site_id, lat, COUNT(DISTINCT(valid_scientific_name)) AS richesse_sp
+    FROM observations
+    INNER JOIN sites ON observations.site_id = sites.site_id
+    WHERE lat > 48 AND lat <= 55
+    GROUP BY sites.site_id
+    ORDER BY lat ASC;"
+  
+  rich.sp.bor <- dbGetQuery(connexion.SQL,request.rich.bor)
+  
+  request.rich.arct <- "
+    SELECT observations.site_id, lat, COUNT(DISTINCT(valid_scientific_name)) AS richesse_sp
+    FROM observations
+    INNER JOIN sites ON observations.site_id = sites.site_id
+    WHERE lat > 55
+    GROUP BY sites.site_id
+    ORDER BY lat ASC;"
+  
+  rich.sp.arct <- dbGetQuery(connexion.SQL,request.rich.arct)
+  
+  return(list(global = rich.sp.tot, tempéré = rich.sp.temp, boréale = rich.sp.bor, arctique = rich.sp.arct))
 
   }
 
