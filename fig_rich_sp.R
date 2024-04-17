8e552f2d70c38bf896dd4930345233da86327c39
XavTheEcologist <quintalx2002@hotmail.com>
2024-04-17 12:08:29 -0400

    modif orthographe

--- fig_rich_sp.R
+++ fig_rich_sp.R
@@ -11,10 +11,10 @@ fig.rich.sp <- function(data.rich){
   r2 <- round(summary(mod.glob)$r.squared, 2)
   text(x = 60.5, y = 37, paste("R² =",r2), cex = 0.8)
   text(x = 60.2, y = 34, paste("y = -2.01x + 133.287"), cex = 0.8)
-  text(x = 46.2, y = 70, paste("Tempéré"), cex = 0.8)
+  text(x = 46.2, y = 70, paste("Tempérée"), cex = 0.8)
   text(x = 53, y = 70, paste("Boréale"), cex = 0.8)
   text(x = 60, y = 69, paste("Arctique"), cex = 0.8)
-  title(main = "Richesse spécifique d'oiseaux observés au Québec en fonction de la latitude")
+  title(main = "Richesse spécifique des oiseaux observés au Québec en fonction de la latitude")
 
 }
 
