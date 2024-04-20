8fa6a37cee9e63c81ad676ce50dc2e5430b9bc68
XavTheEcologist <quintalx2002@hotmail.com>
2024-04-19 22:14:53 -0400

    correction fonction

--- request_abond_rel.R
+++ request_abond_rel.R
@@ -45,35 +45,44 @@ table.abond.rel<-function(connexion.SQL){
   #est-ce que je peux mettre INNER JOIN?
   
   request.nb_esp_tot<-"
-  SELECT COUNT(valid_scientific_name)
+  SELECT COUNT(DISTINCT(valid_scientific_name)) AS nb_sp
   FROM observations; "
   
   nb.esp_total<-dbGetQuery(connexion.SQL,request.nb_esp_tot)
   
-  j <- 1 #nb d'especes
-  k <- 3 #nb de zones?
+   #nb d'especes
+   #nb de zones?
+  
   abondance_relative<-data.frame(zone=" ", espece=" ",abondance_relative=0)
-  for(i in k) {# boucle pour les zones
-   for(j in nb.esp_total) {
-    if (i==1){
+  for(i in 1:3) { # boucle pour les zones
+    n <-1
+    h <-1
+    if (i==1){   
+      for(j in 1: nrow(nb.obs_pour_temp)) {
       abondance_relative[j,1]<-"temperee"
-      abondance_relative[j,2]<-nb.obs_pour_temp[valid_scientific_name[,j]] #mettre le nom des especes dans le data frame
-      abondance_relative[j,3]<-(nb.obs_pour_temp[nombre_observations[,j]])/nb.obs_total*100
+      abondance_relative[j,2]<-nb.obs_pour_temp[j,2] #mettre le nom des especes dans le data frame
+      abondance_relative[j,3]<-(nb.obs_pour_temp[j,3])/nb.obs_total*100
+      }
     }
     else if(i==2){
-      abondance_relative[j,1]<-"boreale"
-      abondance_relative[j,2]<-nb.obs_pour_bor[valid_scientific_name[,j]] #mettre le nom des especes dans le data frame
-      abondance_relative[j,3]<-(nb.obs_pour_bor[nombre_observations[,j]])/nb.obs_total*100
+      for(t in (1+nrow(nb.obs_pour_temp)): (nrow(nb.obs_pour_bor)+nrow(nb.obs_pour_temp))) {
+      abondance_relative[t,1]<-"boreale"
+      abondance_relative[t,2]<-nb.obs_pour_bor[n,2] #mettre le nom des especes dans le data frame
+      abondance_relative[t,3]<-(nb.obs_pour_bor[n,3])/nb.obs_total*100
+      n <-n+1
     }
-    else{
-      abondance_relative[j,1]<-"arctique"
-      abondance_relative[j,2]<-nb.obs_pour_arct[valid_scientific_name[,j]] #mettre le nom des especes dans le data frame
-      abondance_relative[j,3]<-(nb.obs_pour_artc[nombre_observations[,j]])/nb.obs_total*100
     }
-     j<-j+1
+    
+    else if(i==3){
+      for(z in (1+nrow(nb.obs_pour_temp)+nrow(nb.obs_pour_bor)) : (nrow(nb.obs_pour_temp)+nrow(nb.obs_pour_bor)+nrow(nb.obs_pour_arct))) { 
+      abondance_relative[z,1]<-"arctique"
+      abondance_relative[z,2]<-nb.obs_pour_arct[h,2] #mettre le nom des especes dans le data frame
+      abondance_relative[z,3]<-(nb.obs_pour_arct[h,3])/nb.obs_total*100
+      h <-h+1
+      }
    }
-    i<-i+1
-  }
+   }
+  
   
   dbDisconnect(connexion.SQL)
   
