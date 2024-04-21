7c3b307fb7351874072d67527ab6cdd3e732c917
jbreton2 <julianne.breton@usherbrooke.ca>
2024-04-20 22:01:35 -0400

    essai de figure avec toutes les especes

--- fig_ab_rel.R
+++ fig_ab_rel.R
@@ -1,7 +1,7 @@
-fig.abon.rel <- function(data.abond){
+fig.abon.rel <- function(data.abond, zone, espece, abondance.relative){
   
-  couleurs_espece<- colorRampPalette(brewer.pal(8,"Dark2")) (length(levels(data.abond$espece)))
-  plot_ab_rel <- ggplot(data=data.abond, aes(x=Sample, y=Abundance, fill=data.abond$espece))+ 
-  geom_bar(aes(), stat="identity", position="stack")+scale_fill_manual(values = couleurs_espece)
+  #couleurs_espece<- colorRampPalette(brewer.pal(183,"PuBu")) (length(levels(espece)))
+  plot_ab_rel <- ggplot(data=data.abond, aes(x=zone, y=abondance.relative, fill=espece))+ 
+  geom_bar(aes(), stat="identity", position="stack")
   
 }
