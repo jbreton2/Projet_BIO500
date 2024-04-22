aeb4473d2f5ef9a41aec3fdef32cd5716f5f6278
Gaul1914 <gaul1914@usherbrooke.ca>
2024-04-21 21:28:14 -0400

    Ajustement de la figure pour le rapport

--- fig_rich_sp.R
+++ fig_rich_sp.R
@@ -2,19 +2,18 @@
 fig.rich.sp <- function(data.rich){ 
   
   # Figure globale
-  plot(data.rich$global$lat, data.rich$global$richesse_sp, xlab = "Latitude", ylab = "Richesse spécifique", cex=1.1)  
+  plot(data.rich$global$lat, data.rich$global$richesse_sp, xlab = "Latitude", ylab = "Richesse spécifique")  
   
   mod.glob <-lm(richesse_sp~lat, data=data.rich$global)
   abline (mod.glob, col="red")
   abline(v=48)
   abline(v=58)
   r2 <- round(summary(mod.glob)$r.squared, 2)
-  text(x = 60.5, y = 37, paste("R² =",r2), cex = 0.8)
-  text(x = 60.2, y = 34, paste("y = -2.01x + 133.287"), cex = 0.8)
-  text(x = 46.2, y = 70, paste("Tempérée"), cex = 0.8)
-  text(x = 53, y = 70, paste("Boréale"), cex = 0.8)
-  text(x = 60, y = 69, paste("Arctique"), cex = 0.8)
-  title(main = "Richesse spécifique des oiseaux observés au Québec en fonction de la latitude")
+  text(x = 60.5, y = 37, paste("R² =",r2), cex = 0.5)
+  text(x = 60.2, y = 34, paste("y = -2.01x + 133.287"), cex = 0.5)
+  text(x = 46.2, y = 70, paste("Tempérée"), cex = 0.5)
+  text(x = 53, y = 70, paste("Boréale"), cex = 0.5)
+  text(x = 60, y = 69, paste("Arctique"), cex = 0.5)
 
 }
 
